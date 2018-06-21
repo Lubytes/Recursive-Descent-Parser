42f6ce8c3e8f678a891ab7c2a8ea6601674e92c0
John Phillips <jh355248@dal.ca>
2018-06-21 13:52:19 -0300

    Add RDParse.py
    Wrote in code for parse_int, parse_id, and part of parse_listbody

--- RDParse.py
+++ RDParse.py
@@ -25,15 +25,30 @@ def parse_atom(tokenList, index):
 
 
 def parse_list(tokenList, index):
-
+    if(tokenList[index]!='('):
+        print("Syntax Error, impossible state")
+        sys.exit()
+    print()
 
 def parse_listBody(tokenList, index):
 
 
 def parse_id(tokenList, index):
-
+    #Impossible state, return error
+    if(tokenList[index]=='(' or tokenList[index] ==')'):
+        print("Syntax Error, impossible state")
+        sys.exit()
+    print("Token /'{0}/' Production: Atom -> id[{0}]").format(tokenList[index])
+    index = index+1
 
 def parse_int(tokenList, index):
-
+    # Check if token is an integer. If not, print error
+    if(tokenList[index].isdigit()):
+        print("Token /'{0}/' Production: Atom -> int[{0}]").format(tokenList[index])
+        index = index+1
+        #Because this is a terminal, we don't call a further parse function
+    else:
+        print("Syntax Error")
+        sys.exit()
 
 def parse_eps(tokenList, index):
