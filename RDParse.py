00144caba9487edcf1f63c6372c97e82e73e7119
John Phillips <jh355248@dal.ca>
2018-06-21 13:07:38 -0300

    Modify RDParse.py
    Added function headers for parse functions

--- RDParse.py
+++ RDParse.py
@@ -1,12 +1,39 @@
 # File:     RDParse.py
 # Authors:  Brandon Poole, John Phillips, Aqeb Hamdan
 # Date:     June 21st, 2018
-# Purpose:  A recursive descent parser for a specific grammar 
+# Purpose:  A recursive descent parser for a specific grammar
 
 import sys
 
+tokens = []
+count = 0
 file = sys.argv[1]
 fileContents = open(file, "r")
 tokens = fileContents.read().split()
 print(tokens)
-    
+
+
+
+#
+def parse_S(tokenList):
+
+
+def parse_atoms(tokenList, index):
+
+
+def parse_atom(tokenList, index):
+
+
+def parse_list(tokenList, index):
+
+
+def parse_listBody(tokenList, index):
+
+
+def parse_id(tokenList, index):
+
+
+def parse_int(tokenList, index):
+
+
+def parse_eps(tokenList, index):
