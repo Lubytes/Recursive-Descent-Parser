098ae69609a020a6ddb0087ab1b6024b69f3e29b
Brandon <BrandonPoole@live.ca>
2018-06-25 18:16:29 -0300

    Fixed Escape Chars

--- RDParse.py
+++ RDParse.py
@@ -14,7 +14,7 @@ parse_S(tokens)
 
 
 def parse_S(stack):
-    print("Token /'{0}/' Production: S -> Atoms").format(stack[-1])
+    print("Token \'{0}\' Production: S -> Atoms").format(stack[-1])
     parse_atoms(stack)
 
 def parse_atoms(stack):
@@ -22,34 +22,34 @@ def parse_atoms(stack):
         parse_atom(stack)
         parse_atoms(stack)
     else:
-        print("Token /'epsilon/' Production: Atoms -> epsilon")
+        print("Token \'epsilon\' Production: Atoms -> epsilon")
 
 def parse_atom(stack):
     if(stack[-1]==")"):
         print("Syntax Error")
         sys.exit()
     elif(stack[-1]=='\''):
-        print("Token /'{0}/' Production: Atom -> ' Atom").format(stack[-1])
+        print("Token \'{0}\' Production: Atom -> ' Atom").format(stack[-1])
         stack.pop()
         parse_atom(stack)
     elif(stack[-1] == "("):
-        print("Token /'{0}/' Production: Atom -> List").format(stack[-1])
+        print("Token \'{0}\' Production: Atom -> List").format(stack[-1])
         parse_list(stack)
     elif(stack[-1].isdigit()):
-        print("Token /'{0}/' Production: Atom -> int[{0}]").format(stack[-1])
+        print("Token \'{0}\' Production: Atom -> int[{0}]").format(stack[-1])
         stack.pop()
     else:
-        print("Token /'{0}/' Production: Atom -> id[{0}]").format(stack[-1])
+        print("Token \'{0}\' Production: Atom -> id[{0}]").format(stack[-1])
         stack.pop()
 
 def parse_list(stack):
     if(stack[-1]!='('):
         print("Syntax Error")
         sys.exit()
-    print("Token /'{0}/' Production: List -> ( ListBody )").format(stack[-1])
+    print("Token \'{0}\' Production: List -> ( ListBody )").format(stack[-1])
     stack.pop()
     parse_listBody(stack)
 
 def parse_listBody(stack):
-    print("Token /'{0}/' Production: ListBody -> Atoms").format(stack[-1])
+    print("Token \'{0}\' Production: ListBody -> Atoms").format(stack[-1])
     parse_atoms(stack)
